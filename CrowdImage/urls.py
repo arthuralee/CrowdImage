05cb2f27538ab11b3e62e0b6f5cb2e817c8374e3
Jack Montgomery <jackm321@gmail.com>
2014-05-03 23:05:21 -0400

    added url prefix

--- CrowdImage/urls.py
+++ CrowdImage/urls.py
@@ -4,9 +4,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = [
+    url(r'^crowdimage/', include([
+        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    	url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^', include('core.urls')),
-)
+		url(r'^', include('core.urls')),
+    ])),
+]
